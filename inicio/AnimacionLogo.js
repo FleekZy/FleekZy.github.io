fa6040065ee303d3bea27ca870650a17aeb3ee9e
FleekZy <84225203+FleekZy@users.noreply.github.com>
2023-03-18 10:25:26 +0100

    Add files via upload

---
+++ inicio/AnimacionLogo.js
@@ -0,0 +1,8 @@
+const logo = document.querySelector('.logo');
+const shadow = document.querySelector('.shadow');
+
+window.addEventListener('load', () => {
+  logo.classList.add('animate');
+  shadow.classList.add('animate');
+});
+
